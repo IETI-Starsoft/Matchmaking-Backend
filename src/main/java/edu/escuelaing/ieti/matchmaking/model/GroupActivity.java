779787b25e7316e5bfe13bf4a086176f46d75370
diego2097 <ye.yo14@hotmail.com>
2020-03-05 12:27:38 -0500

    :sparkles: Adding activity model

---
+++ src/main/java/edu/escuelaing/ieti/matchmaking/model/GroupActivity.java
@@ -0,0 +1,26 @@
+package edu.escuelaing.ieti.matchmaking.model;
+
+
+public class GroupActivity extends Activity{
+
+     
+    private Equipo team1;
+    private Equipo team2;
+
+    public Equipo getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Equipo team1) {
+        this.team1 = team1;
+    }
+
+    public Equipo getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Equipo team2) {
+        this.team2 = team2;
+    }
+  
+}
---
+++ src/main/java/edu/escuelaing/ieti/matchmaking/model/IndividualActivity.java
@@ -0,0 +1,27 @@
+package edu.escuelaing.ieti.matchmaking.model;
+
+
+public class IndividualActivity extends Activity{
+
+     
+    private User player1;
+    private User player2;
+
+    public User getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(User player1) {
+        this.player1 = player1;
+    }
+
+    public User getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(User player2) {
+        this.player2 = player2;
+    }
+
+  
+}
