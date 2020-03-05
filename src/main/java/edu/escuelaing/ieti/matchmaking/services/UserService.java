9e944ca62b930626b2f433f507e3366a897c3cd5
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-03-05 11:54:52 -0500

    :recycle: Refactor code: User services throw own exception

--- src/main/java/edu/escuelaing/ieti/matchmaking/services/UserService.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/services/UserService.java
@@ -1,18 +1,19 @@
 package edu.escuelaing.ieti.matchmaking.services;
 
+import edu.escuelaing.ieti.matchmaking.exception.MatchmakingException;
 import edu.escuelaing.ieti.matchmaking.model.User;
 
 import java.util.List;
 
 public interface UserService {
 
-    User create(User user);
+    User create(User user) throws MatchmakingException;
 
-    void remove(String userId);
+    void remove(String userId) throws MatchmakingException;
 
-    User update(User user);
+    User update(User user) throws MatchmakingException;
 
-    User getUserById(String userId);
+    User getUserById(String userId) throws MatchmakingException;
 
     List<User> getAll();
 }
