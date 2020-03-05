6ca4bb844097a65d1b6907e3ef1b5671061fc10a
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-03-04 23:40:22 -0500

    :sparkles: New feature: Creation of own exception class

---
+++ src/main/java/edu/escuelaing/ieti/matchmaking/exception/MatchmakingException.java
@@ -0,0 +1,9 @@
+package edu.escuelaing.ieti.matchmaking.exception;
+
+public class MatchmakingException extends Exception {
+
+    public MatchmakingException(String msg) {
+        super(msg);
+    }
+
+}
