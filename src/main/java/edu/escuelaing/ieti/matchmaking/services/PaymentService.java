f8ff929da54c34c5006697fd5c748091e31ced5d
david <cdavidlopez@hotmail.com>
2020-03-11 16:19:34 -0500

    :package: Updating file: Payments model, services/impl

--- src/main/java/edu/escuelaing/ieti/matchmaking/services/PaymentService.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/services/PaymentService.java
@@ -3,10 +3,10 @@ package edu.escuelaing.ieti.matchmaking.services;
 import edu.escuelaing.ieti.matchmaking.exception.MatchmakingException;
 import edu.escuelaing.ieti.matchmaking.model.User;
 
-public interface PagosService {
+public interface PaymentService {
 
-    void rechargeCredits(User user, int saldo) throws MatchmakingException;
+    void rechargeCredits(User user, int amount) throws MatchmakingException;
 
-    void bet(User user, int saldo) throws MatchmakingException;
+    void bet(User user, int amount) throws MatchmakingException;
 
 }
--- src/main/java/edu/escuelaing/ieti/matchmaking/services/impl/PaymentServiceImpl.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/services/impl/PaymentServiceImpl.java
@@ -5,23 +5,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import edu.escuelaing.ieti.matchmaking.exception.MatchmakingException;
 import edu.escuelaing.ieti.matchmaking.model.User;
 import edu.escuelaing.ieti.matchmaking.persistence.UserRepository;
-import edu.escuelaing.ieti.matchmaking.services.PagosService;
+import edu.escuelaing.ieti.matchmaking.services.PaymentService;
 
-public class PagosServiceImpl implements PagosService {
+public class PaymentServiceImpl implements PaymentService {
 
     @Autowired
     UserRepository userRepository;
 
     @Override
-    public void rechargeCredits(User user, int saldo) throws MatchmakingException {
-        user.setCredits(saldo);
+    public void rechargeCredits(User user, int amount) throws MatchmakingException {
+        user.setCredits(amount);
         userRepository.update(user);
-        
+
     }
 
     @Override
-    public void bet(User user, int saldo) throws MatchmakingException {
-        user.setCredits(saldo);
+    public void bet(User user, int amount) throws MatchmakingException {
+        user.setCredits(amount);
         userRepository.update(user);
     }
 
