8eba35eb5e1d95077cda6a7a054ab1bc035704cb
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-03-11 18:25:02 -0500

    :sparkles: New feature: User matchmaking exception

---
+++ src/main/java/edu/escuelaing/ieti/matchmaking/exception/UserMatchmakingException.java
@@ -0,0 +1,9 @@
+package edu.escuelaing.ieti.matchmaking.exception;
+
+public class UserMatchmakingException extends MatchmakingException{
+
+    public UserMatchmakingException(String msg) {
+        super(msg);
+    }
+
+}
