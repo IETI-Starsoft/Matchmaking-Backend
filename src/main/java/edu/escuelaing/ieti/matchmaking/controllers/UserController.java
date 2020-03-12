da34a6520ec5359a8fdc836b697cc0b6d24601cb
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-03-11 19:05:13 -0500

    :sparkles: New feature: User controller

---
+++ src/main/java/edu/escuelaing/ieti/matchmaking/controllers/UserController.java
@@ -0,0 +1,87 @@
+package edu.escuelaing.ieti.matchmaking.controllers;
+
+import edu.escuelaing.ieti.matchmaking.exception.UserMatchmakingException;
+import edu.escuelaing.ieti.matchmaking.model.User;
+import edu.escuelaing.ieti.matchmaking.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private static Logger logger = Logger.getLogger(UserController.class.getName());
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        List<User> users = null;
+        try {
+            users = userService.getAll();
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.log(Level.SEVERE, null, e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable String userId) {
+        User user = null;
+        try {
+            user = userService.getUserById(userId);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (UserMatchmakingException e) {
+            e.printStackTrace();
+            logger.log(Level.INFO, e.getMessage(), e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        User createdUser = null;
+        try {
+            createdUser = userService.create(user);
+            return new ResponseEntity<>(user,HttpStatus.CREATED);
+        } catch (UserMatchmakingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        User updatedUser = null;
+        try {
+            updatedUser = userService.update(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (UserMatchmakingException e) {
+            e.printStackTrace();
+            logger.log(Level.INFO, e.getMessage(), e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> removeUser(@PathVariable String userId) {
+        try {
+            userService.remove(userId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (UserMatchmakingException e) {
+            e.printStackTrace();
+            logger.log(Level.INFO, e.getMessage(), e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
--- src/main/java/edu/escuelaing/ieti/matchmaking/services/UserService.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/services/UserService.java
@@ -1,19 +1,20 @@
 package edu.escuelaing.ieti.matchmaking.services;
 
 import edu.escuelaing.ieti.matchmaking.exception.MatchmakingException;
+import edu.escuelaing.ieti.matchmaking.exception.UserMatchmakingException;
 import edu.escuelaing.ieti.matchmaking.model.User;
 
 import java.util.List;
 
 public interface UserService {
 
-    User create(User user) throws MatchmakingException;
+    User create(User user) throws UserMatchmakingException;
 
-    void remove(String userId) throws MatchmakingException;
+    void remove(String userId) throws UserMatchmakingException;
 
-    User update(User user) throws MatchmakingException;
+    User update(User user) throws UserMatchmakingException;
 
-    User getUserById(String userId) throws MatchmakingException;
+    User getUserById(String userId) throws UserMatchmakingException;
 
     List<User> getAll();
 }
