d4606da73d3f21f0a647307dfb2308968db43689
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-03-25 23:43:58 -0500

    :sparkles: New Feature: Token and User plus get by email
    Co-authored-by: luis572 <luispg572@gmail.com>
    Co-authored-by: diego2097 <ye.yo14@hotmail.com>
    Co-authored-by: cdavidd <cdavidlopez@hotmail.com>

--- src/main/java/edu/escuelaing/ieti/matchmaking/util/Token.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/util/Token.java
@@ -1,19 +1,34 @@
 package edu.escuelaing.ieti.matchmaking.util;
 
+import edu.escuelaing.ieti.matchmaking.model.User;
+
 public class Token {
 
     private String accessToken;
+
+    private User user;
 
     public Token(String accessToken) {
         this.accessToken = accessToken;
     }
 
-
+    public Token(String accessToken, User user) {
+        this.accessToken = accessToken;
+        this.user = user;
+    }
     public String getAccessToken() {
         return accessToken;
     }
 
     public void setAccessToken(String access_token) {
         this.accessToken = access_token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
