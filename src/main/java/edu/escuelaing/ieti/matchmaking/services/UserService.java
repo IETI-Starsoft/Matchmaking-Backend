5631ff5ad9a3e8751518184ca6429d7b9b901f9d
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-03-26 12:42:55 -0500

    :sparkles: New Features: Get user friends and teams

--- src/main/java/edu/escuelaing/ieti/matchmaking/services/UserService.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/services/UserService.java
@@ -2,6 +2,7 @@ package edu.escuelaing.ieti.matchmaking.services;
 
 import edu.escuelaing.ieti.matchmaking.exception.EntityExistsException;
 import edu.escuelaing.ieti.matchmaking.exception.EntityNotFoundException;
+import edu.escuelaing.ieti.matchmaking.model.Team;
 import edu.escuelaing.ieti.matchmaking.model.User;
 
 import java.util.List;
@@ -17,6 +18,10 @@ public interface UserService {
     User getUserById(String userId) throws EntityNotFoundException;
 
     User getUserByEmail(String email) throws EntityNotFoundException;
+
+    List<User> getUserFriendsById(String userId);
+
+    List<Team> getUserTeamsById(String userId);
 
     List<User> getAll();
 }
--- src/main/java/edu/escuelaing/ieti/matchmaking/services/impl/UserServiceImpl.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/services/impl/UserServiceImpl.java
@@ -2,12 +2,16 @@ package edu.escuelaing.ieti.matchmaking.services.impl;
 
 import edu.escuelaing.ieti.matchmaking.exception.EntityExistsException;
 import edu.escuelaing.ieti.matchmaking.exception.EntityNotFoundException;
+import edu.escuelaing.ieti.matchmaking.model.Team;
 import edu.escuelaing.ieti.matchmaking.model.User;
 import edu.escuelaing.ieti.matchmaking.persistence.UserRepository;
+import edu.escuelaing.ieti.matchmaking.services.TeamService;
 import edu.escuelaing.ieti.matchmaking.services.UserService;
 import org.jasypt.util.password.PasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,11 +22,14 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Autowired
+    private TeamService teamService;
+
+    @Autowired
     private PasswordEncryptor passwordEncryptor;
 
     @Override
     public User create(User user) throws EntityExistsException {
-        if (userRepository.existsByEmail(user.getEmail())){
+        if (userRepository.existsByEmail(user.getEmail())) {
             throw new EntityExistsException(User.class, "User email", user.getEmail());
         }
         user.setPassword(passwordEncryptor.encryptPassword(user.getPassword()));
@@ -31,7 +38,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void remove(User user) throws EntityNotFoundException {
-        if (!userRepository.existsByEmail(user.getEmail())){
+        if (!userRepository.existsByEmail(user.getEmail())) {
             throw new EntityNotFoundException(User.class, "User email", user.getEmail());
         }
         userRepository.delete(user);
@@ -39,7 +46,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User update(User user) throws EntityNotFoundException {
-        if (!userRepository.existsByEmail(user.getEmail())){
+        if (!userRepository.existsByEmail(user.getEmail())) {
             throw new EntityNotFoundException(User.class, "User email", user.getEmail());
         }
         return userRepository.save(user);
@@ -55,6 +62,34 @@ public class UserServiceImpl implements UserService {
     public User getUserByEmail(String email) throws EntityNotFoundException {
         Optional<User> optionalUser = userRepository.findByEmail(email);
         return optionalUser.orElseThrow(() -> new EntityNotFoundException(User.class, "User email", email));
+    }
+
+    @Override
+    public List<User> getUserFriendsById(String userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        List<User> friends = new ArrayList<>();
+        optionalUser.ifPresent(user -> {
+            user.getFriends().forEach(friendId -> {
+                        userRepository.findById(friendId).ifPresent(friends::add);
+                    }
+            );
+        });
+        return friends;
+    }
+
+    @Override
+    public List<Team> getUserTeamsById(String userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        List<Team> teams = new ArrayList<>();
+        optionalUser.ifPresent(user -> {
+            user.getTeams().forEach(teamId -> {
+                        try {
+                            teams.add(teamService.getTeamById(teamId));
+                        } catch (EntityNotFoundException ignore) {}
+                    }
+            );
+        });
+        return teams;
     }
 
     @Override
