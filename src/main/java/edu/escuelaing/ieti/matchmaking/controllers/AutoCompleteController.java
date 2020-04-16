2c845839a50ae268d6db3e32606e241be6479751
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-04-15 20:45:36 -0500

    :sparkles: Add a friend & :recycle: Refactor autocomplete

--- src/main/java/edu/escuelaing/ieti/matchmaking/controllers/AutoCompleteController.java
+++ src/main/java/edu/escuelaing/ieti/matchmaking/controllers/AutoCompleteController.java
@@ -1,13 +1,11 @@
 package edu.escuelaing.ieti.matchmaking.controllers;
 
+import edu.escuelaing.ieti.matchmaking.exception.EntityNotFoundException;
 import edu.escuelaing.ieti.matchmaking.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/autocomplete")
@@ -18,9 +16,9 @@ public class AutoCompleteController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
-    public ResponseEntity<?> autocompleteUserSuggestions(@RequestParam String searchstr){
-        return new ResponseEntity<>(userService.getUsersEmailContaining(searchstr, SUGGESTION_LIMIT), HttpStatus.OK);
+    @GetMapping("/users/id/{userId}")
+    public ResponseEntity<?> autocompleteUserSuggestions(@PathVariable String userId, @RequestParam String searchstr) throws EntityNotFoundException {
+        return new ResponseEntity<>(userService.getUsersNotFriendsWithEmailContaining(userId, searchstr, SUGGESTION_LIMIT), HttpStatus.OK);
     }
 
 }
